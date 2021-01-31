247cd90f0e14ac94ac5db10c7dd242203d3551af
宇航 <liuyuhang345@163.com>
2021-01-31 15:18:48 +0800

    加入组件平移

--- 3d_layout/component/monitor_component/README.txt
+++ 3d_layout/component/monitor_component/README.txt
@@ -1,5 +1,6 @@
 js组件，用于提供手动旋转元素的方法。使用格式如下：
-$monitor(selectorMain,selectorObject,flag) ，
+$monitor(selectorMain,selectorObject,axis,isRotate) ，
 第一个参数是用于捕获鼠标坐标的元素；
 第二个是被控制旋转的元素。两个参数都是jquery选择器;
-第三个参数，可以取如下四个值：'x'、'y'、'z'、其它值或省略此参数，分别代表沿着x、y、z和xy轴旋转。
+第三个参数，可以取如下四个值：'x'、'y'、'z'、其它值或省略此参数，分别代表允许的旋转或平移的方向：x、y、z和xy轴旋转。
+第四个参数，rotate表示旋转，translate表示平移	，其它值表示rotate	
--- 3d_layout/component/monitor_component/monitor_component.js
+++ 3d_layout/component/monitor_component/monitor_component.js
@@ -1,4 +1,4 @@
-// rotate 3d-object component for jQuery . help learning .
+// "+act+" 3d-object component for jQuery . help learning .
 // @copy yuhang.liu 刘宇航
 // qq:578384498
 // date 1-28-2021
@@ -11,27 +11,31 @@ var ydeg = 0;
 	var mouseIsDown = false;
 	
 	
-function $watch(selectorMain,selectorObject,flag){
+function $watch(selectorMain,selectorObject,flag,action){
+	var act = action?action:'rotate';
+	var unit = (action=='translate'?'px':'deg');
+	
 	$(selectorMain).on("mousemove",function(e){
 		if(y>-1 && x>-1 && mouseIsDown){
 			xdeg = ydeg + (e.pageY - y) ;
 			ydeg = xdeg + (e.pageX - x) ;
+			
 			if(flag && flag=='x'){
-				$(selectorObject).css("transform","rotateY("+0+"deg) rotateX("+xdeg+"deg)");
+				$(selectorObject).css("transform",act+"Y("+0+unit+") "+act+"X("+xdeg+unit+")");
 			}else if(flag && flag=='y'){
-				$(selectorObject).css("transform","rotateY("+ydeg+"deg) rotateX("+0+"deg)")
+				$(selectorObject).css("transform",act+"Y("+ydeg+unit+") "+act+"X("+0+unit+")")
 			}else if(flag && flag=='z'){
-				$(selectorObject).css("transform","rotateZ("+ydeg+"deg)")
+				$(selectorObject).css("transform",act+"Z("+ydeg+unit+")")
 			}
 			else{
 				if(Math.abs(e.pageY - y) > Math.abs(e.pageX - x)){
-					$(selectorObject).css("transform","rotateY("+(ydeg-(e.pageY-y))+"deg) rotateX("+xdeg+"deg)");
+					$(selectorObject).css("transform",act+"Y("+(ydeg-(e.pageY-y))+unit+") "+act+"X("+xdeg+unit+")");
 				}else{
-					$(selectorObject).css("transform","rotateY("+ydeg+"deg) rotateX("+(xdeg-(e.pageX - x))+"deg)")
+					$(selectorObject).css("transform",act+"Y("+ydeg+unit+") "+act+"X("+(xdeg-(e.pageX - x))+unit+")")
 				}
-				// $(selectorObject).css("transform","rotateY("+ydeg+"deg) rotateX("+xdeg+"deg)")
+				// $(selectorObject).css("transform",act+"Y("+ydeg+unit+") "+act+"X("+xdeg+unit+")")
 			}
-		console.log("x:"+xdeg+"deg;"+"y:"+ydeg+"deg;"+"z:"+ydeg+"deg");
+		console.log("x:"+xdeg+unit+";"+"y:"+ydeg+unit+";"+"z:"+ydeg+unit+"");
 		}
 		y = e.pageY;
 		x = e.pageX;

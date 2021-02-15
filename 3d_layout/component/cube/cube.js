31c4c12cc3fd5dd193a1471f7a97587a0da61af0
宇航 <liuyuhang345@163.com>
2021-02-15 13:49:02 +0800

    cube
    增加了立方体组件

--- 3d_layout/component/cube/cube.js
+++ 3d_layout/component/cube/cube.js
@@ -4,12 +4,74 @@
 // date 2-14-2021
 // email:liuyuhang345@163.com
 
-$cube(selectorMe,height){
-	css_class = $(selectorMe).attr("class");
+
+// 将selectorMe打造成一个长方体，其长宽与selectorMe相等、高度为参数height 。
+function $cube(selectorMe,height,imgs){
+	var e = $(selectorMe);
+	e.css("transform-style","preserve-3d");
 	
-	$(selectorMe).css("transform-style","preserve-3d");
+	var to = e.attr("transform-origin");//保留原来的3d变换中心
+	e.attr("transform-origin","50% 50%");
+	
+	// 创建新的面模板
+	var face = $("<div></div>");
+	var faces=[];
+	
+	// 立方体的长宽高
+	var l = e.css("width");
+	var w = e.css("height");
+	var h = height;
 	
 	
-	 
+	// // 保存老内容
+	var lost_context = e.html();
+	
+	$(selectorMe).html('');//原div充当容器，不再需要内容
+	e.css("backgound-image",'');
+	// clone 6个面,追加到selectorMe元素中
+	for(i =0 ;i<6;i++ ){
+		faces[i] = face.clone();
+		e.append(faces[i]);
+		
+		$(faces[i]).css({
+			"position":"absolute",
+			"background":e.css("background"),
+			"border":e.css("border"),
+			"backgroud-size":"100% 100%",
+			"opacity":"1"
+		})
+	}
+	// 老内容放入第一个面
+	$(faces[0]).html(lost_context);
+
+	// 设置各个面的宽高
+	$(faces[0]).css("width",l).css("height",w).css("background-image","url("+(imgs[0]||'')+")");
+	$(faces[1]).css("width",l).css("height",w).css("background-image","url("+(imgs[1]||imgs[0]||'')+")");
+	$(faces[2]).css("width",l).css("height",h).css("background-image","url("+(imgs[2]||imgs[0]||'')+")");
+	$(faces[3]).css("width",l).css("height",h).css("background-image","url("+(imgs[3]||imgs[2]||imgs[0]||'')+")");
+	$(faces[4]).css("width",w).css("height",h).css("background-image","url("+(imgs[4]||imgs[2]||imgs[0]||'')+")");
+	$(faces[5]).css("width",w).css("height",h).css("background-image","url("+(imgs[5]||imgs[2]||imgs[0]||'')+")");
+	
+	//让各个面居中
+	for(i = 0 ;i<6;i++){
+		$(faces[i]).css({
+			"left":"calc(50% - "+$(faces[i]).css("width")+"/2)",
+			"top":"calc(50% - "+$(faces[i]).css("height")+"/2)"
+		})
+	}
+	
+	// 组成立方体
+	
+	$(faces[0]).css("transform","rotateX(90deg) translateZ(calc("+h+"/2))");
+	$(faces[1]).css("transform","rotateX(270deg) translateZ(calc("+h+"/2))");
+	$(faces[2]).css("transform","translateZ(calc("+w+"/2))");
+	$(faces[3]).css("transform","rotateX(180deg) translateZ(calc("+w+"/2))");
+	$(faces[4]).css("transform","rotateY(90deg) translateZ(calc("+l+"/2))");
+	$(faces[5]).css("transform","rotateY(270deg) translateZ(calc("+l+"/2))");
+	
+	
+	e.attr("transform-origin",to);//恢复3d变换中心 
+	
+	
 	
 }
--- 3d_layout/component/monitor_component/README.txt
+++ 3d_layout/component/monitor_component/README.txt
@@ -1,22 +1,25 @@
-js组件，用于提供手动旋转元素的方法。使用格式如下：
-$monitor(selectorMain,selectorObject,axis,isRotate) ，
-第一个参数是用于捕获鼠标坐标的元素；
-第二个是被控制旋转的元素。两个参数都是jquery选择器;
-第三个参数，可以取如下四个值：'x'、'y'、'z'、其它值或省略此参数，分别代表允许的旋转或平移的方向：x、y、z和xy轴旋转。
-第四个参数，rotate表示旋转，translate表示平移	，其它值表示rotate	
-第四个参数，objectSets是鼠标双击的时候，可以切换过去的3D变换元素的选择器
-
-快捷键：
-x,y,z切换变换的坐标轴；p切换到同时按照x、y轴进行变换
-t（transform）：进行平移变换
-r（rotate）：进行旋转变换
-s（scale）：进行缩放变换
-k（keep）或者鼠标双击本元素 ： 保存当前变换，后续将在保存的变换的基础上进行变换。
-a (axis) ：隐藏坐标轴
-l (location) ：重新设置可（通过鼠标双击）选择的3D对象|元素
-g (animation) ：播放一次旋转动画
-h (help) ：显示此帮助
-c ：查看当前3D变换指令
-鼠标双击某元素，选中变换的元素
-
-注意事项：进行3D变换的时候，objectSets参数中，最好不要出现class属性值完全一致的情况，否则属性一致的元素会混淆历史3D变换。
+<br> <h2>js组件，提供使用鼠标对元素进行3D变换的方法。</h2>
+<hr> <h3>JavaScript API调用格式</h3>
+<br> $watch(selectorMain, transformObject, axis, action, objectSets)
+<br> 第一个参数是用于捕获鼠标坐标的元素；
+<br> 第二个是被控制旋转的元素。两个参数都是jquery选择器;
+<br> 第三个参数，可以取如下四个值：'x'、'y'、'z'、其它值或省略此参数，分别代表允许的旋转或平移的方向：x、y、z和xy轴旋转。
+<br> 第四个参数，rotate表示旋转，translate表示平移	，其它值表示rotate	
+<br> 第五个参数，objectSets是鼠标双击的时候，可以切换过去的3D变换元素的选择器
+<br> 
+<br> <h3>快捷键</h3>
+<br> x,y,z切换变换的坐标轴；p切换到同时按照x、y轴进行变换
+<br> t（transform）：进行平移变换
+<br> r（rotate）：进行旋转变换
+<br> s（scale）：进行缩放变换
+<br> k（keep）或者鼠标双击本元素 ： 保存当前变换，后续将在保存的变换的基础上进行变换。
+<br> a (axis) ：隐藏坐标轴
+<br> l (location) ：重新设置可（通过鼠标双击）选择的3D对象|元素
+<br> g (animation) ：播放一次旋转动画
+<br> h (help) ：显示此帮助
+<br> c ：查看当前3D变换指令
+<br> 鼠标双击某元素，选中变换的元素
+<br> 
+<br> <h3>注意事项</h3>
+<br> 进行3D变换的时候，objectSets参数中，最好不要出现class属性值完全一致的情况，否则属性一致的元素会混淆“上一轮”的3D变换。
+<br> 被选择作为3D变换的对象的class值，必须是唯一的，不允许与其它元素的class值相等，但可以相交。 

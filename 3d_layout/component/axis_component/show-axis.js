b70bfd17e56ee1a99af2c13cb17d7693a4e8183c
宇航 <liuyuhang345@163.com>
2021-02-19 20:38:21 +0800

    dd

--- 3d_layout/component/axis_component/show-axis.js
+++ 3d_layout/component/axis_component/show-axis.js
@@ -22,12 +22,14 @@ function $show_axis(selector) {
 
 //为元素追加3D坐标轴，传入jquery对象
 function $show_axis_Ex(selector_JQuery) {
-	to = selector_JQuery.css("transform-style", "preserve-3d").css("transform-origin").split(" ");
+	to = selector_JQuery
+		.css("transform-style", "preserve-3d")
+		.css("transform-origin").split(" ");
 	x0 = to[0];
 	y0 = to[1];
 
 	// 把坐标轴的外部样式改成行内样式，用于防止用户无意的设置干扰。
-	axis = 'position: absolute;	width: 20cm; height: 2cm;background-repeat: no-repeat;background-size: 100% 100%;'
+	axis = 'position: absolute;	width: 20cm; height: 5cm;background-repeat: no-repeat;background-size: 100% 100%;'
 
 	x = $("<div class='axis x-axis' style='" + axis + "background-image: url(" + getRootPath() + "x-axis.PNG);'></div>");
 	y = $("<div class='axis y-axis' style='" + axis + "background-image: url(" + getRootPath() +
@@ -36,7 +38,7 @@ function $show_axis_Ex(selector_JQuery) {
 		"z-axis.PNG);transform: rotateY(-90deg);'></div>");
 
 	m_left = "calc(" + x0 + " - 10cm)";
-	m_top = "calc(" + y0 + " - 1cm)"; //此处的0.5cm/3cm是坐标轴高度/宽度的一半。其高度在show-axis.css被设定
+	m_top = "calc(" + y0 + " - 2.5cm)"; //此处的0.5cm/3cm是坐标轴高度/宽度的一半。其高度在show-axis.css被设定
 
 	x.css("left", m_left).css("top", m_top);
 	y.css("left", m_left).css("top", m_top);

5ec21b37aa30d9f50b4c85e19be43a4fc4154eec
宇航 <liuyuhang345@163.com>
2021-02-19 17:25:22 +0800

    gg

--- 3d_layout/component/tools.js
+++ 3d_layout/component/tools.js
@@ -4,4 +4,22 @@
 // date 2-14-2021
 // email:liuyuhang345@163.com
 
+// 字符换追加工具，避免用加号连接变量带来的混乱，增加代码可读性
+var StringTools = {
 
+	New: function(str) {
+		strObject = {};
+		strObject.str = str || "";
+		strObject.toString = function() {
+			return strObject.str
+		};
+
+		strObject.append = function(str) {
+			strObject.str = strObject.str + str;
+			return strObject;
+		};
+		
+		return strObject;
+	}
+
+}

e8031f82c1a3ed9ce429f3bb604e275782bd4721
宇航 <liuyuhang345@163.com>
2021-02-28 17:42:53 +0800

    ok2

--- 3d_layout/component/cylinder/README.txt
+++ 3d_layout/component/cylinder/README.txt
@@ -1,9 +1,10 @@
 API:
-function $cylinder(selectorMe, r, height, color, img, level)
+function $cylinder(selectorMe, r, height, color, img, level,scaleModeNum)
 
 selectorMe ,css选择器，在此元素上构建圆柱
 r 圆柱的半径
 height 圆柱的高度
 color 圆柱的颜色
-img 圆柱的材质图像
+img 圆柱的表面贴图
 lever 圆柱的扇面数（3~N）
+scaleModeNum 贴图的伸展模式（1---level个贴图组成，2--- 少量的数个贴图，3---唯一一个贴图伸展铺满柱面），默认3 。
--- 3d_layout/component/cylinder/cylinder.js
+++ 3d_layout/component/cylinder/cylinder.js
@@ -4,9 +4,25 @@
 // date 2-14-2021
 // email:liuyuhang345@163.com
 
-function $cylinder(selectorMe, r, height, color, img, level) {
+function $cylinder(selectorMe, r, height, color, img, level, scaleModeNum) {
 
 	var div = $("<div></div>");
+
+
+	// 求图片在柱子上的展开模式
+	var modeNum = scaleModeNum || '3' ;
+	var mode = "";
+	switch (modeNum) {
+		case '1':
+			mode = "100% 100%";
+			break;
+		case '2':
+			mode = "auto 100%";
+			break;
+		case '3':
+			mode = "calc(2*" + Math.PI + "*" + r + ") " + height;
+			break;
+	}
 
 	var jxs = 2 * Math.sin(Math.PI / level); //每个柱面相对于半径r的的宽度系数
 	div.css({
@@ -14,28 +30,31 @@ function $cylinder(selectorMe, r, height, color, img, level) {
 		"width": StringTools.New("calc(").append(r).append("*").append(jxs).append(")").toString(),
 		"height": height,
 		"background-color": color || "red",
-		"background-image":"url("+img+")",
-		"background-size":"auto "+height,
+		"background-image": "url(" + img + ")",
+		"background-size": mode,
 		"left": StringTools.New("calc(50% - ").append(r).append("*").append(jxs).append("/2").append(")").toString(),
 		"top": StringTools.New("calc(50% - ").append(height).append("/2").append(")").toString(),
-		"overflow":"hidden"
+		"overflow": "hidden"
 	});
 
 
 	$(selectorMe).css("transform-style", "preserve-3d");
-	
+
 	for (i = 0; i < parseInt(level); i++) {
 		var d = div.clone();
 		d.addClass("div_" + (Math.random() + "").replace(".", "_"));
+
 		
-		var mv = StringTools.New("calc(0px - ")
-			.append(r).append("*").append(jxs)
-			.append("*").append(i).append(")")
-			.append(" 0px").toString();
+		if((modeNum=='3') || (modeNum=='2')){
+			var mv = StringTools.New("calc(0px - ")
+				.append(r).append("*").append(jxs)
+				.append("*").append(i).append(")")
+				.append(" 0px").toString();
 			
-		d.css("background-position",mv);
+			d.css("background-position", mv);
+		}
 		
-		
+
 		d.css("transform", "rotateY(" + 360 / level * i + "deg) translateZ(" + r + ")");
 		$(selectorMe).append(d);
 

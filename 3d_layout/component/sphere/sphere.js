f7dd00e38804515c9eecb98795a23ec2afd36cb5
宇航 <liuyuhang345@163.com>
2021-03-05 15:05:25 +0800

    球体完成

--- 3d_layout/component/sphere/sphere.js
+++ 3d_layout/component/sphere/sphere.js
@@ -3,7 +3,7 @@
 // qq:578384498
 // date 2-14-2021
 // email:liuyuhang345@163.com
-
+// 也可以当圆柱组件使用，即当levelH=1 ,则为圆柱;圆柱那个单独的组件，可以删除了。
 
 // 支持选择器集合，每个选中的元素都变成圆柱
 function $sphere(selectorMe, r, color, img, levelW, levelH, util) {
@@ -29,53 +29,59 @@ function $sphere_985665767(selectorMe, r, color, img, levelW, levelH, util) {
 		"position": "absolute",
 		// "background-color": "#" + parseInt(Math.random() * 1000),
 		"background-image": "url(" + img + ")",
-		"background-repeat":"no-repeat",
+		"background-repeat": "no-repeat",
 		"overflow": "hidden"
 	});
 	// 行循环，自赤道开始向上+向下，做半圆,2个
 	for (H = hang; H >= 0; H--) {
 
+		// 本行所占的Y轴高度
+		var height = r / hang;
+
+		// 求本行位移，自赤道开始向上
+		var moveY = (hang - H) * height;
 		// 求本行的半径
 		var lr = r * Math.cos(Math.asin(moveY / r));
 
 		// 分割的每个小区域的宽度、高度
 		var width = 2 * Math.PI * lr / lie;
-		var height = r / hang;
-		
-		// 求本行位移，自赤道开始向上
-		var moveY = (hang - H) * height;
+
 
 		// 设置模板线段的可设置属性
 		xianduan.css({
-			"height": height + util,
-			"width": width + util,
-			"left": "calc(50% - " + width / 2 + util + ")" ,//居中
+			"height": height / Math.cos(Math.asin(moveY / r)) + util,
+			"width": width +2+ util,
+			"left": "calc(50% - " + width / 2 + util + ")", //居中
 			"background-size": 2 * Math.PI * lr + util + " " + 2 * r + util
 		});
 
-		 // $(selectorMe).append(xianduan.clone());// for test
+		// $(selectorMe).append(xianduan.clone());// for test
 		// 列循环
 		for (L = 0; L < lie; L++) {
-			for (i = 0; i < 1; i++) { //循环两次，实现上下两个半圆
+			for (i = 0; i < 2; i++) { //循环两次，实现上下两个半圆:i=0为下半个圆
 				var areaSmall = xianduan.clone();
 				var UP_DOWN = i == 0 ? " + " : " - ";
 				areaSmall.css({
 					"top": "calc(50% - " + height / 2 + util + UP_DOWN + moveY + util + ")",
 				});
+
+
 				// 每次沿着Y轴旋转的小度数
 				var rotateY = 360 / lie * L;
-				// console.log(width * L + util + " " + (0 - r + moveY) + util);
+				var bk_top = (i == 0 ? (0 - r - moveY) : (0 - r + moveY));
+
 				areaSmall.css({
-					"transform": "rotateY(" + rotateY + "deg) translateZ(" + lr + util + ")",
-					"background-position": 0-width * L + util + " " + (0 - r - moveY) + util
+					"transform": "rotateY(" + rotateY + "deg) translateZ(" + lr + util + ")" 
+								+ "rotateX(" + (i==0?"-":" ")+ Math.asin(moveY / r) + "rad)",
+					"background-position": 0 - width * L + util + " " + bk_top + util
 				});
-				
+
 				$(selectorMe).append(areaSmall);
 			}
 
 		}
-		
-		
+
+
 	}
 
 }

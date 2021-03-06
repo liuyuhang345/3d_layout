41820cc4644bf7f2c3002e07b0d4f4eb82f395b0
宇航 <liuyuhang345@163.com>
2021-03-07 02:48:13 +0800

    ok

--- 3d_layout/component/line/line.js
+++ 3d_layout/component/line/line.js
@@ -27,7 +27,7 @@ function $line(selectorMe, point1, point2, color, width, unit) {
 		"position": "absolute", //左端作为原点
 		"background-color": color,
 		"width": width + unit,
-		"height": 3 + unit,
+		"height": 1 + unit,
 		"left": "calc(50% - {0}{1}/2)".format(width, unit),
 		"top": "calc(50% - 0.5{0})".format(unit)
 	};
@@ -44,19 +44,19 @@ function $line(selectorMe, point1, point2, color, width, unit) {
 	var z = 0;
 
 	var maxindex = 0;
-	if (dx > dy && dx > dz) {
-		maxindex = 0;
-	} else if (dy > dx && dy > dz) {
-		maxindex = 1;
-	} else if (dz > dx && dz > dy) {
-		maxindex = 2;
-	} else { //全相等
+	if(dx==dy && dy==dz && dx == 0) { //全相等
 		x = point1[0];
 		y = point1[1];
 		z = point1[2];
 		point.clone().css("transform", "translate3d({0}{3},{1}{3},{2}{3})".format(x, y, z, unit)).appendTo(selectorMe);
 		return;
-	}
+	}else if (dx >= dy && dx >= dz) {
+		maxindex = 0;
+	} else if (dy >= dx && dy >= dz) {
+		maxindex = 1;
+	} else if (dz >= dx && dz >= dy) {
+		maxindex = 2;
+	}  
 
 	var start=Math.min(point1[maxindex],point2[maxindex]);
 	var end = Math.max(point1[maxindex],point2[maxindex]);

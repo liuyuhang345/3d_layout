c1e3d2d262775983f635c5008518317be1f42576
宇航 <liuyuhang345@163.com>
2021-03-09 12:07:06 +0800

    修正了$cube
    增加了调用者自己定义6个面的功能，即增加了一个faces参数

--- 3d_layout/component/cube/cube.js
+++ 3d_layout/component/cube/cube.js
@@ -6,14 +6,14 @@
 
 
 
-function $cube(selectorMe, lenght, width, height, imgs, opacity, contextLocation) {
+function $cube(selectorMe, lenght, width, height, imgs, opacity, contextLocation, faces) {
 	$(selectorMe).each(function() {
-		$cube__my67855688888($(this), lenght, width, height, imgs, opacity, contextLocation);
+		$cube__my67855688888($(this), lenght, width, height, imgs, opacity, contextLocation, faces);
 	})
 }
 
 // 在selectorMe里面打造一个长方体，其长宽高度为参数lenght,width,height，六个面得图像列表为imgs数组，绝对路径（即"/"开头得路径） 。
-function $cube__my67855688888(selectorMe, lenght, width, height, imgs, opacity, contextLocation) {
+function $cube__my67855688888(selectorMe, lenght, width, height, imgs, opacity, contextLocation, faces) {
 	var e = $(selectorMe);
 	e.css("transform-style", "preserve-3d");
 	e.addClass("cube" + (Math.random() + "").replace(/\./g, "_"));
@@ -22,7 +22,7 @@ function $cube__my67855688888(selectorMe, lenght, width, height, imgs, opacity, 
 
 	// 创建新的面模板
 	var face = $("<div></div>");
-	var faces = [];
+	var faces = faces || [];
 
 	// 立方体的长宽高
 	var l = lenght;
@@ -40,18 +40,21 @@ function $cube__my67855688888(selectorMe, lenght, width, height, imgs, opacity, 
 
 	// clone 6个面,追加到selectorMe元素中
 	var id_part = (Math.random() + "").replace(/0\./g, "_");
-	for (i = 0; i < 6; i++) {
-		faces[i] = face.clone();
-		e.append(faces[i]);
+	if (faces.length == 0) {
+		for (i = 0; i < 6; i++) {
+			faces[i] = face.clone();
+			e.append(faces[i]);
 
-		$(faces[i]).css({
-			"position": "absolute",
-			"background": e.css("background"),
-			"border": e.css("border"),
-			"background-size": "100% 100%",
-			"opacity": opacity
-		}).addClass("cubeface_" + id_part + i);
+			$(faces[i]).css({
+				"position": "absolute",
+				"background": e.css("background"),
+				"border": e.css("border"),
+				"background-size": "100% 100%",
+				"opacity": opacity
+			}).addClass("cubeface_" + id_part + i);
+		}
 	}
+
 
 	// e对象无用了，使其不显示在界面上
 	e.css("background-image", 'none').css("background-color", "transparent");
@@ -74,24 +77,27 @@ function $cube__my67855688888(selectorMe, lenght, width, height, imgs, opacity, 
 
 
 	//设置背景图片
-	if (imgs && imgs.join("-").replace(/-/g, '').length > 4) {
-		// if(imgs && imgs[0] && imgs[0].length>0){
-		$(faces[0]).css("background-image", "url(" + (imgs[0] || '') + ")");
-		$(faces[1]).css("background-image", "url(" + (imgs[1] || imgs[0] || '') + ")");
-		$(faces[2]).css("background-image", "url(" + (imgs[2] || imgs[0] || '') + ")");
-		$(faces[3]).css("background-image", "url(" + (imgs[3] || imgs[2] || imgs[0] || '') + ")");
-		$(faces[4]).css("background-image", "url(" + (imgs[4] || imgs[2] || imgs[0] || '') + ")");
-		$(faces[5]).css("background-image", "url(" + (imgs[5] || imgs[2] || imgs[0] || '') + ")");
-	} else {
-		// 设置面的默认颜色
-		$(faces[0]).css("background-color", "red");
-		$(faces[1]).css("background-color", "orange");
-		$(faces[2]).css("background-color", "yellow");
-		$(faces[3]).css("background-color", "green");
-		$(faces[4]).css("background-color", "black");
-		$(faces[5]).css("background-color", "blue");
+	if(!faces){//调用者没有提供6个面，即faces参数
+		if (imgs && imgs.join("-").replace(/-/g, '').length > 4) {
+			// if(imgs && imgs[0] && imgs[0].length>0){
+			$(faces[0]).css("background-image", "url(" + (imgs[0] || '') + ")");
+			$(faces[1]).css("background-image", "url(" + (imgs[1] || imgs[0] || '') + ")");
+			$(faces[2]).css("background-image", "url(" + (imgs[2] || imgs[0] || '') + ")");
+			$(faces[3]).css("background-image", "url(" + (imgs[3] || imgs[2] || imgs[0] || '') + ")");
+			$(faces[4]).css("background-image", "url(" + (imgs[4] || imgs[2] || imgs[0] || '') + ")");
+			$(faces[5]).css("background-image", "url(" + (imgs[5] || imgs[2] || imgs[0] || '') + ")");
+		} else {
+			// 设置面的默认颜色
+			$(faces[0]).css("background-color", "red");
+			$(faces[1]).css("background-color", "orange");
+			$(faces[2]).css("background-color", "yellow");
+			$(faces[3]).css("background-color", "green");
+			$(faces[4]).css("background-color", "black");
+			$(faces[5]).css("background-color", "blue");
+		}
+		
 	}
-
+	
 
 
 	//让各个面居中
--- 3d_layout/component/tools/tools.js
+++ 3d_layout/component/tools/tools.js
@@ -35,6 +35,20 @@ $(function() {
 
 	});
 	
+// 用于组件开发中，求得当前被应用得js或css的路径
+function getImportRootPath(js_css_filename) {
+	var js = document.scripts || document.getElementsByTagName("script");
+	var jsPath;
+	for (var i = js.length; i > 0; i--) {
+		// if (js[i - 1].src.indexOf("import.js") > -1) {
+		if (js[i - 1].src.indexOf(js_css_filename) > -1) {
+			jsPath = js[i - 1].src.substring(0, js[i - 1].src.lastIndexOf("/") + 1);
+		}
+	}
+	return jsPath;
+}
+
+
 // 字符换追加工具，避免用加号连接变量带来的混乱，增加代码可读性
 var StringTools = {
 
@@ -64,20 +78,6 @@ String.prototype.add=function(str){
 }
 
 
-// 用于组件开发中，求得当前被应用得js或css的路径
-function getImportRootPath(js_css_filename) {
-	var js = document.scripts || document.getElementsByTagName("script");
-	var jsPath;
-	for (var i = js.length; i > 0; i--) {
-		// if (js[i - 1].src.indexOf("import.js") > -1) {
-		if (js[i - 1].src.indexOf(js_css_filename) > -1) {
-			jsPath = js[i - 1].src.substring(0, js[i - 1].src.lastIndexOf("/") + 1);
-		}
-	}
-	return jsPath;
-}
-
-
 // 实现占位符
 String.prototype.format = function () {
     // 数据长度为空，则直接返回
@@ -91,3 +91,29 @@ String.prototype.format = function () {
     }
 	return result;
 };
+
+
+// 进入全屏
+  function $launchFullscreen(element) {
+    if(element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if(element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if(element.webkitRequestFullscreen) {
+		alert()
+      element.webkitRequestFullscreen();
+    } else if(element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
+	
+  }
+  // 退出全屏
+  function $exitFullscreen() {
+    if(document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if(document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if(document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
